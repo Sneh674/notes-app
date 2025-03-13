d33f8a445b993fb397b666aa88201c87e37cf74f
Sneh674 <naiksneh6@gmail.com>
2025-03-13 19:34:20 +0530

    added otp and expdate in user model

--- models/logged.js
+++ models/logged.js
@@ -1,25 +1,27 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // mongoose.connect("mongodb://127.0.0.1:27017/testapp2");
-const mongoURL=process.env.DB_URL
-mongoose.connect(mongoURL)
+const mongoURL = process.env.DB_URL;
+mongoose.connect(mongoURL);
 // mongoose.connect("mongodb+srv://naiksneh6:snehnischal@notes-user-db.plfxt.mongodb.net/notes-app")
 // retryWrites=true&w=majority&appName=notes-user-db
 
-const logSchema=mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})
+const logSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  otp: { type: String, required: true, default: "" },
+  expiresAt: { type: Date, required: true, default: Date.now },
+});
 
-module.exports=mongoose.model('loggeduser', logSchema);
+module.exports = mongoose.model("loggeduser", logSchema);
